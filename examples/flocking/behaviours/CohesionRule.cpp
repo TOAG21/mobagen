7f6a6a94e4e6b4cc0ca15e1f9eab359cfd960251
Samuel Heasley <samuel.heasley@mymail.champlain.edu>
2025-09-03 13:21:37 -0400

    base flocking rules implemented

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -3,11 +3,22 @@
 
 Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   Vector2f cohesionForce;
+  Vector2f boidAgentPos = boid->getPosition();
 
-  // todo: add your code here to make a force towards the center of mass
-  // hint: iterate over the neighborhood
+  if (neighborhood.size() < 1) 
+  {
+    Vector2f zero(0.0f, 0.0f);
+    return zero;
+  }
 
-  // find center of mass
+  Vector2f cohesionCenterMass;
+  for (int i = 0; i < neighborhood.size(); i++) {
+      cohesionCenterMass += neighborhood[i]->getPosition();
+  }
+  cohesionCenterMass.x /= neighborhood.size();
+  cohesionCenterMass.y /= neighborhood.size();
 
+    // get force vector from center of mass
+    cohesionForce = cohesionCenterMass - boidAgentPos;
   return cohesionForce;
 }
